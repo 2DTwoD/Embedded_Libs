41a2961e503695e0bee333bb116ed21afabc8d32
Dmitry <113849847+2DTwoD@users.noreply.github.com>
2024-10-04 16:19:37 +0500

    development

--- Control/all/src/pid.cpp
+++ Control/all/src/pid.cpp
@@ -6,78 +6,78 @@ PIDreg::PIDreg(uint16_t t, float sp, float kp, float ti, float td,
 	updateKoef();
 	reset();
 }
-uint16_t PIDreg::getT(){
+uint16_t PIDreg::getT() const{
 	return t;
 }
 void PIDreg::setPv(float value){
 	pv = value;
 	updateSomewhere();
 }
-float PIDreg::getPv(){
+float PIDreg::getPv() const{
 	return pv;
 }
 void PIDreg::setSp(float value){
 	sp = value;
 }
-float PIDreg::getSp(){
+float PIDreg::getSp() const{
 	return sp;
 }
 void PIDreg::setKp(float value){
 	kp = value;
 	updateKoef();
 }
-float PIDreg::getKp(){
+float PIDreg::getKp() const{
 	return kp;
 }
 void PIDreg::setTi(float value){
 	ti = value;
 	updateKoef();
 }
-float PIDreg::getTi(){
+float PIDreg::getTi() const{
 	return ti;
 }
 void PIDreg::setTd(float value){
 	td = value;
 	updateKoef();
 }
-float PIDreg::getTd(){
+float PIDreg::getTd() const{
 	return td;
 }
 void PIDreg::setDb(float value){
 	db = abs(value);
 }
-float PIDreg::getDb(){
+float PIDreg::getDb() const{
 	return db;
 }
 void PIDreg::setUpOutLim(float value){
 	upOutLim = max(value, downOutLim);
 }
-float PIDreg::getUpOutLim(){
+float PIDreg::getUpOutLim() const{
 	return upOutLim;
 }
 void PIDreg::setDownOutLim(float value){
 	downOutLim = min(value, upOutLim);
 }
-float PIDreg::getDownOutLim(){
+float PIDreg::getDownOutLim() const{
 	return downOutLim;
 }
 void PIDreg::setOut(float value){
 	if(AUTO) return;
 	out = limit(value, downOutLim, upOutLim);
 }
-float PIDreg::getOut(){
+float PIDreg::getOut() const{
 	return out;
 }
 void PIDreg::setAuto(bool value){
 	AUTO = value;
 }
-bool PIDreg::isAUTO(){
+bool PIDreg::isAUTO() const{
 	return AUTO;
 }
 void PIDreg::setInverse(bool value){
 	inverse = value;
 }
-bool PIDreg::isInverse(){
+bool PIDreg::isInverse() const{
 	return inverse;
 }
 void PIDreg::updateSomewhere(){
